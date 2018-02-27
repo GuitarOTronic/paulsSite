5e4cc93f1ebf5749c06251cc713826795c3ea5e6
Sean Obringer <SeanObringer90@gmail.com>
2018-02-26 21:24:11 -0800

    resized arrow. added CNAME file

--- src/components/policies/policies.js
+++ src/components/policies/policies.js
@@ -7,23 +7,24 @@ class Policies extends Component {
       <div className='policiesContainer'>
         <h1>Policies</h1>
         <div className='policyContent'>
-          <p>In an effort to keep payments timely, encourage lesson regularity, and avoid complicated reschedules all students are required to agree to and sign the Payment Policy, Cancellation Policy, and Lesson Credit System.
+          <p>
+            In an effort to keep payments timely, encourage lesson regularity, and avoid complicated reschedules all students are required to agree to and sign the Payment Policy, Cancellation Policy, and Lesson Credit System.
 
-              Monthly Payment Policy
-              Lesson payments will be submitted on a monthly basis. Payment for the coming month will be submitted during the last lesson of the current month and is non-refundable.  Any known conflicts that would result in a lesson cancellation for the coming month should be discussed at that time and charges will be adjusted accordingly.
+            Monthly Payment Policy
+            Lesson payments will be submitted on a monthly basis. Payment for the coming month will be submitted during the last lesson of the current month and is non-refundable.  Any known conflicts that would result in a lesson cancellation for the coming month should be discussed at that time and charges will be adjusted accordingly.
 
-              Lesson Credit System
-              The Lesson Credit System will work in tandem with the Same-Day Cancellation Policy and the Monthly Payment Policy. In the event that a cancellation occurs before the day of the scheduled lesson, a reschedule will be attempted. If a reschedule cannot be arranged, the payment for that lesson will be credited to the next month. The cost of one lesson will be removed from the next month’s payment. If a reschedule is successfully arranged and is cancelled for any reason, the cost of that lesson will not be credited.
+            Lesson Credit System
+            The Lesson Credit System will work in tandem with the Same-Day Cancellation Policy and the Monthly Payment Policy. In the event that a cancellation occurs before the day of the scheduled lesson, a reschedule will be attempted. If a reschedule cannot be arranged, the payment for that lesson will be credited to the next month. The cost of one lesson will be removed from the next month’s payment. If a reschedule is successfully arranged and is cancelled for any reason, the cost of that lesson will not be credited.
 
-              Reschedule Cancellation Policy
-              Cancellation by student: If a student cancels a rescheduled lesson, for any reason, credit will not be provided for the cancellation
+            Reschedule Cancellation Policy
+            Cancellation by student: If a student cancels a rescheduled lesson, for any reason, credit will not be provided for the cancellation
 
-              Cancellation by teacher: If I cancel a rescheduled lesson, for any reason, credit will be given for the cancellation.
+            Cancellation by teacher: If I cancel a rescheduled lesson, for any reason, credit will be given for the cancellation.
 
-              Same-Day Cancellation Policy
-              Cancellation by student: If a student cancels a lesson on the scheduled day, for any reason, credit will not be provided for the cancellation.
+            Same-Day Cancellation Policy
+            Cancellation by student: If a student cancels a lesson on the scheduled day, for any reason, credit will not be provided for the cancellation.
 
-              Cancellation by teacher: If I cancel a lesson on the scheduled day, for any reason, a reschedule will be attempted. If we cannot agree upon a date/time, the lesson will be credited to the next month.
+            Cancellation by teacher: If I cancel a lesson on the scheduled day, for any reason, a reschedule will be attempted. If we cannot agree upon a date/time, the lesson will be credited to the next month.
 
 
             </p>
